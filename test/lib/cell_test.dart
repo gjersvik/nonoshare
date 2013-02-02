b9a934cf14ca8a61e0f18c30ec8d4d5a9778a085
Ole Martin Gjersvik <olem@olem.org>
2013-02-02 12:32:23 +0100

    Cell: Static property mark returns Cell [fail]

--- test/lib/cell_test.dart
+++ test/lib/cell_test.dart
@@ -2,5 +2,10 @@ import 'package:unittest/unittest.dart';
 import 'package:nonoshare/nonoshare.dart';
 
 main(){
-  
+  group('Cell:',() {
+    test('Static property mark returns Cell',() {
+      var mark = Cell.mark;
+      expect(mark,predicate((o) => o is Cell, 'is a Cell'));
+    });
+  });
 }
