e8dd1f2ea24afae21410a027d766044f11bc6986
Ole Martin Gjersvik <olem@olem.org>
2013-02-04 10:41:09 +0100

    FAIL: Cell: Mark toString returns X

--- test/lib/cell_test.dart
+++ test/lib/cell_test.dart
@@ -5,9 +5,8 @@ import 'package:nonoshare/nonoshare.dart';
 
 main(){
   group('Cell:',() {
-    test('Static property mark returns MarkedCell',() {
-      var mark = Cell.mark;
-      expect(mark,predicate((o) => o is MarkedCell, 'is a MarkedCell'));
+    test('Mark toString returns X',() {
+      expect(Cell.mark.toString(),'X');
     });
   });
 }
