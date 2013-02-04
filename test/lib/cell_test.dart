419bac1befea05e2a4826366aace9675ee9126ea
Ole Martin Gjersvik <olem@olem.org>
2013-02-04 10:55:35 +0100

    FAIL: Cell: Empty toString returns ?

--- test/lib/cell_test.dart
+++ test/lib/cell_test.dart
@@ -8,8 +8,11 @@ main(){
     test('Mark toString returns X',() {
       expect(Cell.mark.toString(),'X');
     });
-    test('Blank toString returns X',() {
+    test('Blank toString returns _',() {
       expect(Cell.blank.toString(),'_');
+    });
+    test('Empty toString returns ?',() {
+      expect(Cell.empty.toString(),'?');
     });
   });
 }
